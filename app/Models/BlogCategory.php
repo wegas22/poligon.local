672cf5c7f7f32ff5967a7d7cc63457739684f74a
Alex <wegas22@yandex.ru>
2019-10-27 23:29:18 +0400

    comit

--- app/Models/BlogCategory.php
+++ app/Models/BlogCategory.php
@@ -3,8 +3,15 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class BlogCategory extends Model
 {
-    //
+    use SoftDeletes;
+    protected $fillable = [
+        'title',
+        'slug',
+        'parent_id',
+        'description',
+    ];
 }
--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,22 @@ Route::group(['namespace' => 'Blog', 'prefix' => 'blog'], function (){
     Route::resource('posts', 'PostController')->names('blog.posts');
 });
 
-Route::resource('rest', 'RestTestController')->names('restTest');
+//Route::resource('rest', 'RestTestController')->names('restTest');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+
+//>Admin
+$groupData = [
+    'namespace' => 'Blog\Admin',
+    'prefix' => 'admin/blog',
+];
+Route::group($groupData, function (){
+    $methods = ['index', 'edit', 'update', 'create', 'store'];
+    Route::resource('categories', 'CategoryController')
+        ->only($methods)
+        ->names('blog.admin.categories');
+});
+//<
