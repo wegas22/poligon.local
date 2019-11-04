cd4af1890cc6d32ad72e1e54c3eb8aa6106f9f6d
Alex <wegas22@yandex.ru>
2019-11-04 17:27:37 +0400

    database

---
+++ database/seeds/BlogCategoriesTableSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class BlogCategoriesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $categories = [];
+        $cName = 'Без категории';
+        $categories[]=[
+            'title' => $cName,
+            'slug' => str_slug($cName),
+            'parent_id' => 0,
+        ];
+
+        for($i=2; $i<=11; $i++){
+            $cName = 'Категория #'. $i;
+            $parentId = ($i>4)?rand(1,4):1;
+            $categories[]=[
+                'title' => $cName,
+                'slug' => str_slug($cName),
+                'parent_id' => $parentId,
+            ];
+        }
+        \DB::table('blog_categories') -> insert($categories);
+    }
+}
