834ebd4320d02e2578e24aeebec07f6cb337f154
Alex <wegas22@yandex.ru>
2019-11-04 15:55:25 +0400

    commit 02.11

---
+++ app/Repositories/BlogPostRepository.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\BlogPost as Model;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Pagination\LengthAwarePaginator;
+
+/**
+ * Class BlogCategoryRepository
+ * @package App\Repositories
+ */
+
+class BlogPostRepository extends CoreRepository
+{
+    /**
+     * @return string
+     */
+    protected function getModelClass()
+    {
+        return Model::class;
+    }
+
+    /**
+     * Получить список всех статей в админке
+     *
+     * @return LengthAwarePaginator
+     */
+    public function getAllWithPaginate()
+    {
+        $columns = [
+            'id',
+            'title',
+            'slug',
+            'is_published',
+            'published_at',
+            'user_id',
+            'category_id'
+        ];
+
+        $result = $this->startConditions()
+            ->select($columns)
+            ->orderBy('id', 'DESC')
+            ->with([
+                'category' => function($query) {
+                    $query->select(['id', 'title']);
+                },
+                'user:id,name'])
+            ->paginate(25);
+
+        return $result;
+    }
+}
